cdc75b2215eeaaee8c1af11edf8c5ea867ff219d
Hashka <hugo.krysztof@gmail.com>
2022-02-25 15:13:52 +0100

    25/02/2022

--- TP2/app/src/main/java/cnam/smb116/tp2/MainActivity.java
+++ TP2/app/src/main/java/cnam/smb116/tp2/MainActivity.java
@@ -6,8 +6,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,68 +16,48 @@ import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText tp2Text;
-    private ImageButton callBtn;
-    private ImageButton loginBtn;
-    private ImageButton internetBtn;
+    private EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initializeView();
+        configureView();
     }
 
-    private void initializeView(){
-        this.tp2Text = findViewById(R.id.tp2Text);
-        this.callBtn = findViewById(R.id.callBtn);
-        this.loginBtn = findViewById(R.id.loginBtn);
-        this.internetBtn = findViewById(R.id.internetBtn);
+    private void configureView(){
+        this.inputText = findViewById(R.id.inputText);
     }
 
     public void onClickCallBtn(View view){
-        String url = "tel:"+tp2Text.getText().toString();
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+inputText.getText().toString()));
         startActivity(intent);
     }
 
     public void onClickInternetBtn(View view){
-        String url = "http://www."+tp2Text.getText().toString()+".com";
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www."+inputText.getText().toString()+".com"));
         startActivity(intent);
     }
 
     public void onClickLogin(View view){
         Intent intent = new Intent();
 
+//        //Intent explicite
+//        String pkg = "cnam.smb116.tp2_composantlogin";
+//        String clazz = pkg + ".MainActivity";
+//        intent.setComponent(new ComponentName(pkg,clazz));
+//
+//        //Intent implicite
+//        intent.setAction("login.ACTION");
+//        startActivityForResult(intent, 1);
 
         if (ContextCompat.checkSelfPermission(this, "com.example.permission.CALL_LOGIN") == PackageManager.PERMISSION_GRANTED) {
-            //Permission already granted!
-            String pkg = "cnam.smb116.tp2_composantlogin";
-            String clazz = pkg + ".MainActivity";
-            intent.setComponent(new ComponentName(pkg,clazz));
-
+            intent.setComponent(new ComponentName("cnam.smb116.tp2_composantlogin", "cnam.smb116.tp2_composantlogin.MainActivity"));
             startActivityForResult(intent, 1);
-
-//        }if (ActivityCompat.shouldShowRequestPermissionRationale(this, "com.example.permission.CALL_LOGIN")) {
-//            //Display a screen saying why you need PERMISSION_X
-//            Toast.makeText(getApplicationContext(),"The permission is needed to launch the next activity!",Toast.LENGTH_LONG).show();
-
         }
         else {
             ActivityCompat.requestPermissions(this, new String[]{"com.example.permission.CALL_LOGIN"}, 2);
         }
-
-        //implicite
-//        intent.setAction("login.ACTION");
-
-        //explicite
-//        String pkg = "com.example.tp2login";
-//        String clazz = pkg + ".MainActivity";
-//        intent.setComponent(new ComponentName(pkg,clazz));
-//
-//        startActivityForResult(intent, 1);
     }
 
     @Override
@@ -85,32 +65,25 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 1 && data != null){
-            Toast.makeText(getApplicationContext(), data.getStringExtra("reponse"),Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), data.getStringExtra("feedback"),Toast.LENGTH_SHORT).show();
         }
         if (requestCode == -1 && data != null){
-            Toast.makeText(getApplicationContext(), data.getStringExtra("reponse"),Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), data.getStringExtra("feedback"),Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        switch (requestCode) {
-            case 2:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission is granted. Continue the action or workflow
-                    Intent intent = new Intent();
-                    String pkg = "cnam.smb116.tp2_composantlogin";
-                    String clazz = pkg + ".MainActivity";
-                    intent.setComponent(new ComponentName(pkg,clazz));
-
-                    startActivityForResult(intent, 1);
-                }  else {
-                    Toast.makeText(getApplicationContext(),"The permission is needed to launch the next activity!",Toast.LENGTH_LONG).show();
-                }
-                return;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == 2) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("cnam.smb116.tp2_composantlogin", "cnam.smb116.tp2_composantlogin.MainActivity"));
+                startActivityForResult(intent, 1);
+            } else {
+                Toast.makeText(getApplicationContext(), "The permission is needed to launch the next activity!", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
--- TP2_ComposantLogin/app/src/main/java/cnam/smb116/tp2_composantlogin/MainActivity.java
+++ TP2_ComposantLogin/app/src/main/java/cnam/smb116/tp2_composantlogin/MainActivity.java
@@ -2,7 +2,6 @@ package cnam.smb116.tp2_composantlogin;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,38 +10,30 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText userName;
     private EditText password;
-    private Button cancel;
-    private Button login;
-
-    private int RESULT_OK = 1;
-    private int RESULT_CANCELED = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         configureView();
     }
 
     private void configureView(){
-        this.userName = findViewById(R.id.userNameTxt);
-        this.password = findViewById(R.id.passwordTxt);
-        this.cancel = findViewById(R.id.cancelBtn);
-        this.login = findViewById(R.id.loginBtn);
+        this.userName = findViewById(R.id.txtUname);
+        this.password = findViewById(R.id.txtPwd);
     }
 
     public void onClickLogin(View view){
         Intent intent = new Intent();
-        intent.putExtra("reponse", "user: "+userName.getText().toString()+" password: "+password.getText().toString());
-        setResult(RESULT_OK, intent);
+        intent.putExtra("feedback", "User Name: "+userName.getText().toString()+" Password: "+password.getText().toString());
+        setResult(1, intent);
         finish();
     }
 
     public void onClickCancel(View view){
         Intent intent = new Intent();
-        intent.putExtra("reponse", "canceled opération");
-        setResult(RESULT_CANCELED, intent);
+        intent.putExtra("feedback", "Annulation");
+        setResult(-1, intent);
         finish();
     }
 }
